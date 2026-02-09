7fa50984204ca4f9ad6066952ba62d2aac8a6379
ohad83 <ohad306@gmail.com>
2026-02-09 09:16:55 +0000

    edit app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from EC2 Jenkins pipeline! lets move on");
+    res.end("Hello from EC2 Jenkins pipeline! lets move on aaaaaaaa");
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
